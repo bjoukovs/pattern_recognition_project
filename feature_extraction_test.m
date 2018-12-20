b89ddbaf32ae5bf0e7bb346bdb2ae7496af9b61c
bjoukovs <borisjoukov@hotmail.com>
2018-12-20 13:30:11 +0100

    small correction

--- feature_extraction_test.m
+++ feature_extraction_test.m
@@ -11,7 +11,7 @@ PCA_mapping = feature_extraction(train, false, 0.95, 0)
 PCA_mapping2 = feature_extraction(train, false, 0, 50)
 
 %PCA + Fishermapping based on retvar for PCA
-PCAFISHER_mapping = feature_extraction(train, false, 0.95, 0)
+PCAFISHER_mapping = feature_extraction(train, true, 0.95, 0)
 
 %PCA + Fishermapping based on number of classes
-PCAFISHER_mapping = feature_extraction(train, false, 0, 50)
+PCAFISHER_mapping = feature_extraction(train, true, 0, 50)
