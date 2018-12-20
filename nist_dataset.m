2dda0f5418d61e70a05cc93f34ae00994be81f28
bjoukovs <borisjoukov@hotmail.com>
2018-12-20 13:25:52 +0100

    extracting data

--- nist_dataset.m
+++ nist_dataset.m
@@ -3,7 +3,32 @@ clear all, close all, prwaitbar off;
 % The NIST datasets contains 1000 images per classes
 NIST = prnist([0:9], [1:1000]);
 
+images_per_class = {};
 
+classes = 1:10;
+labels = 0:9;
+
+for i=classes
+    
+    fprintf('Extracting images for class %d \n', i);
+    images_per_class{i} = data2im(seldat(NIST,i));
+    
+end
+
+save('IMAGES.mat', 'images_per_class');
+
+%%
+
+%Step 1 : Manipulate the images, resize them, make them square...
+
+
+
+
+%Step 2 : making a new dataset using the vectorized images and prdataset()
+
+
+
+%Step 3 : Making the big and small datasets
 %Case 1: Big training set
 
 [DATA1, DATA2] = gendat(NIST,0.5);
