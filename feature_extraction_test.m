368184c0eee3a9c813607a4a95bd144379f060b6
bjoukovs <borisjoukov@hotmail.com>
2018-12-29 20:21:45 +0100

    Feature extraction and evaluation, image resizing

--- feature_extraction_test.m
+++ feature_extraction_test.m
@@ -4,14 +4,77 @@ dataset = load('datasets/big_dataset.mat');
 train = dataset.train_big;
 tst = dataset.tst_big;
 
+% dataset = load('datasets/small_dataset.mat');
+% train = dataset.train_small;
+% tst = dataset.tst_small;
+
+
 %PCA Mapping based on retvar criterion
 PCA_mapping = feature_extraction(train, false, 0.95, 0)
 
 %PCA Mapping based on number of classes
-PCA_mapping2 = feature_extraction(train, false, 0, 50)
+%PCA_mapping2 = feature_extraction(train, false, 0, 50)
 
 %PCA + Fishermapping based on retvar for PCA
 PCAFISHER_mapping = feature_extraction(train, true, 0.95, 0)
 
 %PCA + Fishermapping based on number of classes
-PCAFISHER_mapping = feature_extraction(train, true, 0, 50)
+PCAFISHER_mapping2 = feature_extraction(train, true, 0, 100)
+
+%%
+
+% Showing the 5 first components of the pca mapping
+figure;
+scatterd(train*PCA_mapping(:,1:5), 'gridded');
+
+% Showing the 5 first components of PCA+Fisher (retvar method)
+figure
+scatterd(train*PCAFISHER_mapping(:,1:5), 'gridded');
+
+% Showing the 5 first components of PCA+Fisher (N method)
+figure
+scatterd(train*PCAFISHER_mapping2(:,1:5), 'gridded');
+
+%%
+
+%Evaluation of the PCA mapping method
+figure
+EPCA_NMC = clevalf(train*PCA_mapping, nmc, [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
+EPCA_PARZEN = clevalf(train*PCA_mapping, parzenc([],0.5), [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
+EPCA_FISHER = clevalf(train*PCA_mapping, fisherc, [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
+EPCA_KNN = clevalf(train*PCA_mapping, knnc([], 1), [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
+plote({EPCA_NMC, EPCA_PARZEN, EPCA_FISHER, EPCA_KNN});
+
+%Evaluation of the PCA+Fisher mapping
+figure
+EPCAF = clevalf(train*PCAFISHER_mapping, nmc, 1:9, [],  1, tst*PCAFISHER_mapping);
+EPCAF_PARZEN = clevalf(train*PCAFISHER_mapping, nmc, 1:9, [], 1, tst*PCAFISHER_mapping);
+EPCAF_FISHER = clevalf(train*PCAFISHER_mapping, fisherc, 1:9, [], 1, tst*PCAFISHER_mapping);
+EPCAF_KNN = clevalf(train*PCAFISHER_mapping, knnc([], 1), 1:9, [], 1, tst*PCAFISHER_mapping);
+plote({EPCAF, EPCAF_PARZEN, EPCAF_FISHER, EPCAF_KNN});
+
+%Evaluation on PCA+Fisher (reduced N)
+figure
+EPCAF2 = clevalf(train*PCAFISHER_mapping2, nmc, 1:9, [],  1, tst*PCAFISHER_mapping2);
+EPCAF2_PARZEN = clevalf(train*PCAFISHER_mapping2, nmc, 1:9, [], 1, tst*PCAFISHER_mapping2);
+EPCAF2_FISHER = clevalf(train*PCAFISHER_mapping2, fisherc, 1:9, [], 1, tst*PCAFISHER_mapping2);
+EPCAF2_KNN = clevalf(train*PCAFISHER_mapping2, knnc([], 1), 1:9, [], 1, tst*PCAFISHER_mapping2);
+plote({EPCAF2, EPCAF2_PARZEN, EPCAF2_FISHER, EPCAF2_KNN});
+
+
+%% CONCLUSIONS
+
+% CONCLUSION: Pour le BIG Dataset
+%    - Globalement, la meilleure méthode est PCA + fisher avec N limité. ça
+%    permet d'avoir un minimum d'erreur à seulement 6 features pour à peut
+%    près tous les classifiers (un peu moins de 20% d'erreur)
+
+%    - Il y a une exception: 1-NN fonctionne SUPER bien avec le simple PCA,
+%    à environ 120 features. L'erreur de classification est inférieure à
+%    10%. Par contre dans ce cas là tous les autres classifiers font du
+%    caca
+
+
+% CONCLUSION: Pour le SMALL Dataset
+%    Etonnament je n'observe pas de résultats très différents par rapport
+%    aux cas précédent
--- nist_dataset.m
+++ nist_dataset.m
@@ -1,30 +1,13 @@
 clear all, close all, prwaitbar off;
 
 % The NIST datasets contains 1000 images per classes
-NIST = prnist([0:9], [1:1000]);
+% Puts them in a box and resize it to 64x64
+% The datasetm command needs equal size images to work
 
-images_per_class = {};
+NIST = datasetm(im_resize(im_box(prnist([0:9], [1:1000]), 0, 1), [64 64]));
 
-classes = 1:10;
-labels = 0:9;
-
-for i=classes
-    
-    fprintf('Extracting images for class %d \n', i);
-    images_per_class{i} = data2im(seldat(NIST,i));
-    
-end
-
-save('IMAGES.mat', 'images_per_class');
-
-%%
 
 %Step 1 : Manipulate the images, resize them, make them square...
-
-
-
-
-%Step 2 : making a new dataset using the vectorized images and prdataset()
 
 
 
@@ -49,7 +32,7 @@ save("datasets/big_dataset.mat", 'train_big', 'tst_big');
 
 save("datasets/small_nist_eval.mat", 'DATA2');
 
-frac = 0.1 %10 images for training, 90 for testing
+frac = 0.1; %10 images for training, 90 for testing
 [train_small, tst_small] = gendat(DATA1, frac);
 
 save("datasets/small_dataset.mat", 'train_small', 'tst_small');
