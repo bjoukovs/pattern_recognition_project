8a042396b824d3ce776812efcee61445ae23cf8b
bjoukovs <borisjoukov@hotmail.com>
2019-01-26 16:30:31 +0100

    tests for feature extraction

--- Testing/feature_extraction_test.m
+++ Testing/feature_extraction_test.m
@@ -1,5 +1,8 @@
 clear all, close all, prwaitbar off;
 
+addpath("../")
+addpath("utils/")
+
 dataset = load('datasets/big_dataset.mat');
 train = dataset.train;
 tst = dataset.tst;
@@ -19,7 +22,7 @@ PCA_mapping = feature_extraction(train, false, 0.95, 0)
 PCAFISHER_mapping = feature_extraction(train, true, 0.95, 0)
 
 %PCA + Fishermapping based on number of classes
-PCAFISHER_mapping2 = feature_extraction(train, true, 0, 100)
+PCAFISHER_mapping2 = feature_extraction(train, true, 0, 30)
 
 %%
 
@@ -43,23 +46,29 @@ EPCA_NMC = clevalf(train*PCA_mapping, nmc, [1,2,3,4,5,10:5:100], [], 1, tst*PCA_
 EPCA_PARZEN = clevalf(train*PCA_mapping, parzenc([],0.5), [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
 EPCA_FISHER = clevalf(train*PCA_mapping, fisherc, [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
 EPCA_KNN = clevalf(train*PCA_mapping, knnc([], 1), [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
-plote({EPCA_NMC, EPCA_PARZEN, EPCA_FISHER, EPCA_KNN});
+EPCA_LDC = clevalf(train*PCA_mapping, ldc, [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
+EPCA_QDC = clevalf(train*PCA_mapping, qdc, [1,2,3,4,5,10:5:100], [], 1, tst*PCA_mapping);
+plote({EPCA_NMC, EPCA_PARZEN, EPCA_FISHER, EPCA_KNN, EPCA_LDC, EPCA_QDC});
+%%
 
 %Evaluation of the PCA+Fisher mapping
 figure
 EPCAF = clevalf(train*PCAFISHER_mapping, nmc, 1:9, [],  1, tst*PCAFISHER_mapping);
-EPCAF_PARZEN = clevalf(train*PCAFISHER_mapping, nmc, 1:9, [], 1, tst*PCAFISHER_mapping);
+EPCAF_PARZEN = clevalf(train*PCAFISHER_mapping, parzenc([],0.5), 1:9, [], 1, tst*PCAFISHER_mapping);
 EPCAF_FISHER = clevalf(train*PCAFISHER_mapping, fisherc, 1:9, [], 1, tst*PCAFISHER_mapping);
 EPCAF_KNN = clevalf(train*PCAFISHER_mapping, knnc([], 1), 1:9, [], 1, tst*PCAFISHER_mapping);
 plote({EPCAF, EPCAF_PARZEN, EPCAF_FISHER, EPCAF_KNN});
 
+%%
 %Evaluation on PCA+Fisher (reduced N)
 figure
 EPCAF2 = clevalf(train*PCAFISHER_mapping2, nmc, 1:9, [],  1, tst*PCAFISHER_mapping2);
-EPCAF2_PARZEN = clevalf(train*PCAFISHER_mapping2, nmc, 1:9, [], 1, tst*PCAFISHER_mapping2);
+EPCAF2_PARZEN = clevalf(train*PCAFISHER_mapping2, parzenc([],0.5), 1:9, [], 1, tst*PCAFISHER_mapping2);
 EPCAF2_FISHER = clevalf(train*PCAFISHER_mapping2, fisherc, 1:9, [], 1, tst*PCAFISHER_mapping2);
 EPCAF2_KNN = clevalf(train*PCAFISHER_mapping2, knnc([], 1), 1:9, [], 1, tst*PCAFISHER_mapping2);
-plote({EPCAF2, EPCAF2_PARZEN, EPCAF2_FISHER, EPCAF2_KNN});
+EPCAF2_LDC = clevalf(train*PCAFISHER_mapping2, ldc, 1:9, [], 1, tst*PCAFISHER_mapping2);
+EPCAF2_QDC = clevalf(train*PCAFISHER_mapping2, qdc, 1:9, [], 1, tst*PCAFISHER_mapping2);
+plote({EPCAF2, EPCAF2_PARZEN, EPCAF2_FISHER, EPCAF2_KNN, EPCAF2_LDC, EPCAF2_QDC});
 
 
 %% CONCLUSIONS
