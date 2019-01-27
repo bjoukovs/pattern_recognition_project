0195525c276c9698b19130c8589892e8c7487b0f
FadiDahdouh <dahdouhfadi@hotmail.com>
2019-01-28 00:15:44 +0100

    Other classifiers are added to gen_classifier_big as comments + some extra graphs

--- utils/gen_classifier_big.m
+++ utils/gen_classifier_big.m
@@ -15,6 +15,15 @@ function [classfr, test_error] = gen_classifier_big(train, tst)
     % Bagging QDC 100
     bagging_c  = baggingc(mapped_train, qdc, 100);
     
+    % SVC with d^3 kernel 
+%     svc_c = svc(proxm('d',3))*fisherc;
+%     svc_c = svc_c(mapped_train);
+    
+    % Neural network combination of QDC and KN1
+    % features must be changed to 24 instead of 30
+%     combi_c = [knnc([],1)*classc qdc*classc]*bpxnc([],10,5000);
+%     combi_c = combi_c(mapped_train);
+    
     classfr = PCA_mapping(:,1:30)*bagging_c;
     
     [E C] = tst*classfr*testc;
